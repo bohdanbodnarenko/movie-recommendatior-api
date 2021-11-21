2acb05c7ee2138f60ba934ad8005a0e253df0865
Bohdan Bodnarenko <bohdan.bodnarenko@crello.com>
2021-11-21 19:40:11 +0200

    added needed routes

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+
+from recommendations import MovieRecommender
 
 app = Flask(__name__)
+CORS(app)
+
+
+def get_dataset_abs_path(dataset: str) -> str:
+    return os.path.join(os.path.dirname(__file__), 'datasets', dataset)
+
+
+recommender = MovieRecommender(get_dataset_abs_path('tmdb_5000_movies.csv'),
+                               get_dataset_abs_path('tmdb_5000_credits.csv'),
+                               get_dataset_abs_path('ratings_small.csv'))
+
+
+@app.route('/movie', methods=['GET'])
+def recommend_movies():
+    title = request.args.get('title')
+    user_id = request.args.get('user_id')
+    skip = request.args.get('skip')
+    limit = request.args.get('limit')
+
+    try:
+        res = recommender.recommend(movie_title=title, user_id=user_id, skip=skip,
+                                    limit=limit)
+        return jsonify(res)
+    except ValueError:
+        abort(400)
+    except KeyError:
+        abort(404)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Go to /movie-recommendation for check the app'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
